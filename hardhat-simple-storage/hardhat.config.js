e8acefe1c13579ffe47c3d6b6b751cadd97aa553
Prime <prime79inovative@gmail.com>
2022-09-18 11:55:48 +0530

    up

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,40 @@
-require("@nomicfoundation/hardhat-toolbox");
+const { task } = require('hardhat/config')
+const nomiclabsPlugin = require('solidity-coverage')
+
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
+require('@nomiclabs/hardhat-etherscan')
+//require('@nomiclabs/hardhat-waffle')
+require('./tasks/block-number')
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const Goerli_RPC = process.env.Goerli_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
-  solidity: "0.8.9",
-};
+    defaultNetwork: 'hardhat',
+    networks: {
+        Goerli: {
+            url: Goerli_RPC,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: 'http://127.0.0.1:8545/',
+            chainId: 31337,
+        },
+    },
+    solidity: '0.8.9',
+    etherscan: {
+        apiKey: API_KEY,
+    },
+    gasReporter: {
+      enabled: true,
+      outputFile: 'gas-report.txt',
+      noColors: true,
+      currency: 'USD',
+      coinmarketcap: 'process.env.COIN_API_KEY',
+    }
+}
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,31 +1,52 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+//imports
+const { exec, ethers } = require('hardhat')
+const { run, network } = require('hardhat')
 
+//async main
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        'SimpleStorage'
+    )
+    console.log('Depoying contract .......')
+    const simplestorage = await SimpleStorageFactory.deploy()
+    await simplestorage.deployed()
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+    console.log(`Deployed contract address: ${simplestorage.address}`)
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        console.log('Waiting for verification..')
+        await simplestorage.deployTransaction.wait(3)
+        await verify(simplestorage.address, [])
+    }
+    const currentValue = await simplestorage.retrieve()
+    console.log(`Current value: ${currentValue}`)
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    //update current value
+    const transactionResponse = await simplestorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simplestorage.retrieve()
+    console.log(`Updated value: ${updatedValue}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+    console.log('Verifying contract on Etherscan...')
+    try {
+        await run('verify:verify', {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (error) {
+        if (error.message.toLowerCase().includes('already verified')) {
+            console.log('Contract already verified')
+        } else {
+            //console.log(error)
+        }
+    }
+}
+
+//main
+main()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err)
+        process.exit(1)
+    })
